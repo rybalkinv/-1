86edfab83a3a9a4c402aa72366e3334617124b73
Vladimir <rybalkinv@inbox.ru>
2023-03-12 17:18:31 +0300

    введен масив

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+string[] strTestArray = new string[8]{"Five","Inspect", "free", "2", "4", "no", "cap", "ball"};
+string[] strArray;
+string text = string.Empty;
+
+
+string[] InputStringArray(string textArg)                          
+                                    
+{
+    var data = textArg.Split(",") 
+                .Select(e => e = e.Replace(" ", string.Empty))
+                                                                                 
+                .ToArray(); 
+
+    return data;
+}
+
+
+void printArray(string[] arr)
+{
+    string strPrint = string.Empty;
+
+    
+        for(int i = 0; i < arr.Length; i++)
+        {
+            strPrint = strPrint + arr[i] + ", ";    
+        }
+   
+    Console.WriteLine(strPrint);
+}
